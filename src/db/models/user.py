973e4d29d0b4089189a709d3c05525d32841b83a
elik <yeelisey@gmail.com>
2025-07-23 16:29:45 +0500

    chore: add user model

---
+++ src/db/models/user.py
@@ -0,0 +1,24 @@
+import datetime
+from sqlalchemy import String, DateTime, Boolean, BigInteger
+from sqlalchemy.orm import Mapped, mapped_column
+from src.db.base import Base
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    platform: Mapped[str] = mapped_column(String(50))
+    platform_user_id: Mapped[str] = mapped_column(String(128), index=True)
+
+    first_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    last_name: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    username: Mapped[str | None] = mapped_column(String(100), nullable=True)
+    phone_number: Mapped[str | None] = mapped_column(String(32), nullable=True)
+
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    is_admin: Mapped[bool] = mapped_column(Boolean, default=False)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime, default=datetime.datetime.utcnow
+    )
